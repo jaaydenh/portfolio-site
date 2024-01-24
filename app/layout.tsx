1addd1a2cee0b00d999cd5b03c632538acd698f2
Jaayden Halko <jaayden.halko@gmail.com>
2024-01-23 21:04:39 -0500

    add more metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,33 @@ import '@/app/ui/global.css';
 import { Analytics } from '@vercel/analytics/react';
 import { GeistSans } from 'geist/font/sans';
 import { Metadata } from 'next';
+import type { Viewport } from 'next';
+
+export const viewport: Viewport = {
+  themeColor: 'black',
+};
 
 export const metadata: Metadata = {
   title: 'Jaayden Halko',
   description:
-    'Jaayden Halko is Software Engineer who build products for the modern web.',
+    'Jaayden Halko is Software Engineer who build products for the modern web',
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Jaayden Halko',
+    description:
+      'Jaayden Halko is Software Engineer who build products for the modern web',
+    site: '@jaaydenh',
+    creator: '@jaaydenh',
+  },
+  openGraph: {
+    title: 'Jaayden Halko',
+    description:
+      'Jaayden Halko is Software Engineer who build products for the modern web',
+    url: 'https://jaayden.com',
+    siteName: 'Jaayden Halko',
+    locale: 'en_US',
+    type: 'website',
+  },
 };
 
 export default function RootLayout({
